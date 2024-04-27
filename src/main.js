599cdd574ecd5222cbad5ba431b6b1ffb4d7a596
Felipe Oliveira <f.limadeoliveira@dxc.com>
2024-04-27 13:08:50 -0300

    Alterações e adição de links

--- src/main.js
+++ src/main.js
@@ -3,3 +3,15 @@ import './css/output.css'
 document.querySelector('#comp-menu').innerHTML = `
 
 `
+document.querySelectorAll('.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+        window.scrollTo({
+            top: target.offsetTop - 50,
+            behavior: 'smooth'
+        });
+    });
+});
+
