ff4e44bd888e4d72cf8917121021b887eefe5366
Miguel <miguelmcontato@gmail.com>
2024-08-27 15:05:04 -0300

    Add Selector Dark Mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{html,js}"],
+  darkMode: 'class', // ou 'media' para detectar preferências do sistema
   theme: {
     extend: {}
   },
